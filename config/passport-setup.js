9a4aaa7f963c36c3fab92327efd6b4c145393616
3dw1nM0535 <workockmoses@gmail.com>
2017-10-23 14:14:48 +0300

    redefined passport configuration

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,14 +1,15 @@
 var passport = require('passport'); 
-var GoogleStrategy = require('passport-google-oauth20');
+var GoogleStrategy = require('passport-google-oauth2').Strategy;
 var authConfig = require(process.cwd() + '/config/auth');
 
 //Set up passport Middleware
 passport.use(
   new GoogleStrategy({
-    clientID: authConfig.google.clientId,
-    clientSecret:authConfig.google.clientSecret 
-  }),
+    clientSecret: 'authConfig.google.clientSecret',
+    clientID: 'authConfig.google.clientID',
+    callbackUrl: '/auth/google/redirect'
+  },
   function () {
 
-  }
+  })
 );
