ecead92a26951988618c577d9037cc23af0cacf1
3dw1nM0535 <workockmoses@gmail.com>
2017-10-23 14:15:50 +0300

    final code factory edit

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   google: {
-    clientId: 132949391198-s5gre8cr74p2ajisahmfqqd1n2oc7vtd.apps.googleusercontent.com,
-    clientSecret: OzqeDYKEbNLlC955sCWDovh0
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
   }
 };
