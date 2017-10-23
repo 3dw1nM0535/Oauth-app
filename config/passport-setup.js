48c2b474ca5d94686f432c9f9d7b95315272190c
3dw1nM0535 <workockmoses@gmail.com>
2017-10-23 12:46:52 +0300

    imported config modules

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,10 +1,14 @@
 var passport = require('passport'); 
 var GoogleStrategy = require('passport-google-oauth20');
+var authConfig = require(process.cwd() + '/config/auth');
 
 //Set up passport Middleware
 passport.use(
-  new GoogleStrategy({}),
+  new GoogleStrategy({
+    clientID: authConfig.google.clientId,
+    clientSecret:authConfig.google.clientSecret 
+  }),
   function () {
-    
+
   }
 );
