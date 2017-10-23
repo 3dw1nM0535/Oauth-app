cfbdb72232fdde4b691ed284e1edb08596f574f6
3dw1nM0535 <workockmoses@gmail.com>
2017-10-23 16:43:45 +0300

    redefined access keys

--- config/auth.js
+++ config/auth.js
@@ -1,8 +1,8 @@
-//Define authentication configuration and export the module to use in other files
+//Module exports for sensitive key for Oauth
 
 module.exports = {
-  google: {
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
+  github: {
+    clientID: '77e8e85b5cb596ad3d59',
+    clientSecret: 'b947f516e8fd74a530e9ae1ffdc14a9e81b4d3f3'
   }
 };
