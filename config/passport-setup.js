f4831ff7e5309b7b52206cc882b50d6a471a198d
3dw1nM0535 <workockmoses@gmail.com>
2017-10-23 20:55:19 +0300

    add newUSer to the db

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,15 +1,25 @@
 var passport = require('passport');
 var GithubStrategy = require('passport-github2').Strategy;
 var authConfig = require(process.cwd() + '/config/auth');
+var User = require(process.cwd() + '/models/model');
 
 //Set up passport Middleware
 passport.use(
   new GithubStrategy({
-    clientSecret: authConfig.github.clientSecret,
-    clientID: authConfig.github.clientID,
-    callbackURL: 'http://localhost:3000/auth/github/callback'
-  },
-  function (accessToken, refreshToken, profile,done) {
-    console.log(profile); //Console.log() output to the console
-  })
+      clientSecret: authConfig.github.clientSecret,
+      clientID: authConfig.github.clientID,
+      callbackURL: 'http://localhost:3000/auth/github/callback'
+    },
+    function(accessToken, refreshToken, profile, done) {
+      //console.log(profile); //Console.log() output to the console
+      var newUser = new User();
+
+      newUser.id = profile.id;
+      newUser.username = profile.username;
+      newUser.displayName = profile.displayName;
+
+      newUser.save().then(function(newUser) {
+        console.log(newUser);
+      });
+    })
 );
