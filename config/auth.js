fec654dade46edfab0d212571ecdf23d7125f9dc
3dw1nM0535 <workockmoses@gmail.com>
2017-10-23 12:47:27 +0300

    google key config

---
+++ config/auth.js
@@ -0,0 +1,8 @@
+//Define authentication configuration and export the module to use in other files
+
+module.exports = {
+  google: {
+    clientId: 132949391198-s5gre8cr74p2ajisahmfqqd1n2oc7vtd.apps.googleusercontent.com,
+    clientSecret: OzqeDYKEbNLlC955sCWDovh0
+  }
+};
