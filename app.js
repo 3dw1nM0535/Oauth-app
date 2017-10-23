b235c1246623a848476fd645b1e62e3fe2e2c198
3dw1nM0535 <workockmoses@gmail.com>
2017-10-23 23:31:45 +0300

    Middleware for deprecated mongoose.Promise

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 var express = require('express'); //import express module
 var authRoutes = require(process.cwd() + '/routes/auth-routes');
 var passportSetup = require(process.cwd() + '/config/passport-setup');
+var profileRoutes = require(process.cwd() + '/routes/profile-routes');
 var mongoose = require('mongoose');
-var keys = require(process.cwd() + '/config/auth');
+var keys = require(process.cwd() + '/config/keys');
 var passport = require('passport');
+var session = require('cookie-session');
+
+//db connection instance
 mongoose.connect(keys.mongodb.dburi, {
   useMongoClient: true
 }, function() {
   console.log('Connected to mongodb successfully!');
 });
 
+//mongoose Promise Middleware
+mongoose.Promise = global.Promise;
+
 var app = express(); //instantiate express object
+
+//session Middleware
+app.use(session({
+  keys: [keys.session.cookieKey],
+  maxAge: 24 * 60 * 60 * 1000
+}));
 
 //passport Middleware
 app.use(passport.initialize());
@@ -18,6 +31,7 @@ app.use(passport.session());
 
 
 app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
 
 //Static files Middleware
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -31,7 +45,7 @@ app.set('view engine', 'ejs');
 
 //get home route
 app.get('/', function(req, res) {
-  res.render('home');
+  res.render('home', { user: req.user });
 });
 
 
